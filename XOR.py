10748ee20abb90eb0f06364f6b941b17979a558f
Fülöp Bence <59794495+fulopbencus@users.noreply.github.com>
2024-05-24 10:11:32 +0200

    Complicated algoryth

--- XOR.py
+++ XOR.py
@@ -1,24 +1,28 @@
-# Minden karaktert bitenként XOR-olja a kulccsal, így előállítva az enkripciót.
+# Minden karaktert bitenként XOR-olja a kulccsal és az egyes karaktereket különböző kulcsértékekkel XOR-olja ráadásul, így előállítva az enkripciót.
 # Author Fülöp Bence
 
 def xor_encrypt(text, key):
     encrypted_text = ""
-    for char in text:
-        encrypted_text += chr(ord(char) ^ key)
+    key_length = len(key)
+    for i, char in enumerate(text):
+        current_key = ord(key[i % key_length])
+        encrypted_text += chr(ord(char) ^ current_key)
     return encrypted_text
 
 def xor_decrypt(encrypted_text, key):
     decrypted_text = ""
-    for char in encrypted_text:
-        decrypted_text += chr(ord(char) ^ key)
+    key_length = len(key)
+    for i, char in enumerate(encrypted_text):
+        current_key = ord(key[i % key_length])
+        decrypted_text += chr(ord(char) ^ current_key)
     return decrypted_text
 
 password = input("Enter your password: ")
-key = 256
+key = input("Enter your key: ")
 
 encrypted_password = xor_encrypt(password, key)
 print("Encrypted Password:", encrypted_password)
 
 # Ellenőrzés:
-#decrypted_password = xor_decrypt(encrypted_password, key)
-#print("Decrypted Password:", decrypted_password)
+decrypted_password = xor_decrypt(encrypted_password, key)
+print("Decrypted Password:", decrypted_password)
